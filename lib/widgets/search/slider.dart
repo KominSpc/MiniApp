a567092e150d34fb83ac74799c5209a128ed77b5
Komin <komin.spyds@gmail.com>
2026-09-13 18:28:44 +0800

    轮播图完成

--- lib/widgets/search/slider.dart
+++ lib/widgets/search/slider.dart
@@ -4,13 +4,16 @@ import 'package:mini_app/viewmodels/slider_pic.dart';
 
 class SearchPageSlider extends StatefulWidget {
   List<SliderPic> sliderPics = [];
-  SearchPageSlider({required this.sliderPics, super.key});
 
+  SearchPageSlider({required this.sliderPics, super.key});
   @override
   State<SearchPageSlider> createState() => _SearchPageSliderState();
 }
 
 class _SearchPageSliderState extends State<SearchPageSlider> {
+  int _onTapIndexNow = 0;
+  CarouselSliderController _sliderController = CarouselSliderController();
+
   List<Widget> _createCarouselSliderItems() {
     List<Widget> lists = List.generate(widget.sliderPics.length, (index) {
       return Image.network(
@@ -22,15 +25,59 @@ class _SearchPageSliderState extends State<SearchPageSlider> {
     return lists;
   }
 
-  @override
-  Widget build(BuildContext context) {
+  CarouselSlider _createCarouselSlider() {
     return CarouselSlider(
+      carouselController: _sliderController,
       items: _createCarouselSliderItems(),
       options: CarouselOptions(
         viewportFraction: 1,
         autoPlay: true,
         autoPlayInterval: Duration(seconds: 4),
+        onPageChanged: (index, reason) {
+          _onTapIndexNow = index;
+          setState(() {});
+        },
       ),
+    );
+  }
+
+  List<Widget> _createSliderDirector() {
+    return List.generate(widget.sliderPics.length, (index) {
+      return GestureDetector(
+        onTap: () {
+          _onTapIndexNow = index;
+          _sliderController.animateToPage(_onTapIndexNow);
+          setState(() {});
+        },
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 100),
+          height: 6,
+          width: _onTapIndexNow == index ? 80 : 50,
+          decoration: BoxDecoration(
+            color: _onTapIndexNow == index ? Colors.redAccent : Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _createCarouselSlider(),
+        Positioned(
+          bottom: 10,
+          left: 0,
+          right: 0,
+          child: Row(
+            spacing: 30,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _createSliderDirector(),
+          ),
+        ),
+      ],
     );
   }
 }
